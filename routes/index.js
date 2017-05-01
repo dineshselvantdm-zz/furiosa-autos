45b65e2449071788deab46baddd4c2e1fd07fffb
Dinesh Tamilselvan <dineshselvantdm@gmail.com>
2017-05-01 14:15:00 +0530

    Working responsive demo

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var api = require('../lib/api');
 
+//Utility modules for each route 
+var models = require('../lib/utils/models.js');
+var services = require('../lib/utils/services.js');
+var reviews = require('../lib/utils/reviews.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index');
@@ -13,7 +18,11 @@ router.get('/', function(req, res, next) {
 */
 router.get('/models', function(req, res, next) {
 	// use api to get models and render output
-	res.render('models');
+	api.fetchModels().then(function(data){
+		var sortedModels = models.getSortedData(data,req);
+		res.render('models',{models:sortedModels});
+	});
+	
 });
 
 /*
@@ -22,7 +31,10 @@ router.get('/models', function(req, res, next) {
 */
 router.get('/services', function(req, res, next) {
 	// use api to get services and render output
-	res.render('services');
+	api.fetchServices().then(function(data){
+		var filteredServices = services.getFilteredData(data,req);
+		res.render('services',{services:filteredServices});
+	});
 });
 
 /*
@@ -30,10 +42,16 @@ router.get('/services', function(req, res, next) {
 * Bugfix: Something prevents reviews from being rendered
 * Make reviews searchable (content and source)
 */
+
+/*Fix: Merged customer and corporate reviews array using 
+* Array.prototype.reduce function.
+*/
 router.get('/reviews', function(req, res, next) {
 	return Promise.all([api.fetchCustomerReviews(), api.fetchCorporateReviews()])
-		.then(function(reviews) {
-			res.render('reviews', {reviews: reviews});
+		.then(function(data) {
+			var searchedReviews = reviews.getSearchedData(data,req);
+			var searchKey = reviews.getSearchKey(req);
+			res.render('reviews', {reviews: searchedReviews,searchKey: searchKey});
 		});
 });
 
